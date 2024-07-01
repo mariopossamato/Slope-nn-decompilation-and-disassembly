f45e170a1fe571ed30e5b7987cebab70e152fa32
Mario Possamato <51346942+mariopossamato@users.noreply.github.com>
2024-07-01 01:37:06 -0400

    Update PaneBase.hxx

--- include/nn/ui2d/detail/PaneBase.hxx
+++ include/nn/ui2d/detail/PaneBase.hxx
@@ -1,12 +1,15 @@
 #ifndef NN_UI2D_DETAIL_PANEBASE_HXX
 #define NN_UI2D_DETAIL_PANEBASE_HXX
 
+#include "nn/util/IntrusiveListNode.hxx"
+
 namespace nn::ui2d::detail {
 struct PaneBase {
   PaneBase();
   PaneBase(PaneBase const&) = delete;
   PaneBase& operator=(PaneBase const&) = delete;
   virtual ~PaneBase();
+  nn::util::IntrusiveListNode m_Link;
 };
 } // end namespace nn::ui2d::detail
 
