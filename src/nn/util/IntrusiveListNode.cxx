000b51edbf59a2a71226b038277d329aca7046e3
Mario Possamato <51346942+mariopossamato@users.noreply.github.com>
2024-07-02 23:57:09 -0400

    Update IntrusiveListNode.cxx

--- src/nn/util/IntrusiveListNode.cxx
+++ src/nn/util/IntrusiveListNode.cxx
@@ -2,3 +2,36 @@
 
 nn::util::IntrusiveListNode::IntrusiveListNode() {
 }
+
+bool nn::util::IntrusiveListNode::IsLinked() const {
+}
+
+nn::util::IntrusiveListNode* nn::util::IntrusiveListNode::GetPrev() {
+}
+
+const nn::util::IntrusiveListNode* nn::util::IntrusiveListNode::GetPrev() const {
+}
+
+nn::util::IntrusiveListNode* nn::util::IntrusiveListNode::GetNext() {
+}
+
+const nn::util::IntrusiveListNode* nn::util::IntrusiveListNode::GetNext() const {
+}
+
+void nn::util::IntrusiveListNode::LinkPrev(nn::util::IntrusiveListNode* pNode) {
+}
+
+void nn::util::IntrusiveListNode::LinkPrev(nn::util::IntrusiveListNode* pFirst, nn::util::IntrusiveListNode* pLast) {
+}
+
+void nn::util::IntrusiveListNode::LinkNext(nn::util::IntrusiveListNode* pNode) {
+}
+
+void nn::util::IntrusiveListNode::LinkNext(nn::util::IntrusiveListNode* pFirst, nn::util::IntrusiveListNode* pLast) {
+}
+
+void nn::util::IntrusiveListNode::Unlink() {
+}
+
+void nn::util::IntrusiveListNode::Unlink(nn::util::IntrusiveListNode* pLast) {
+}
