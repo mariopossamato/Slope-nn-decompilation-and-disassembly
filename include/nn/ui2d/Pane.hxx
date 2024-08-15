fac82355550a59d1c7ef892f9b3880f996c0708c
Mario Possamato <51346942+mariopossamato@users.noreply.github.com>
2024-08-15 02:34:47 -0400

    Update Pane.hxx

--- include/nn/ui2d/Pane.hxx
+++ include/nn/ui2d/Pane.hxx
@@ -1,3 +1,11 @@
 #ifndef NN_UI2D_PANE_HXX
 #define NN_UI2D_PANE_HXX
+
+#include "nn/ui2d/detail/PaneBase.hxx"
+
+namespace nn::ui2d {
+class Pane : public nn::ui2d::detail::PaneBase {
+};
+} // namespace nn::ui2d
+
 #endif
